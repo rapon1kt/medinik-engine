5ceb13e1011b628752a09f0f4a4d8b32e57332ea
rapon1kt <shinnatensei@gmail.com>
2025-05-15 18:23:48 -0300

    feat: add resizeble and close properties to window

--- medinik-engine/main.cpp
+++ medinik-engine/main.cpp
@@ -1,18 +1,20 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode({512, 512}), "Medinik Engine", sf::Style::Close | sf::Style::Titlebar);
+	sf::RenderWindow window(sf::VideoMode({512, 512}), "Medinik Engine", sf::Style::Close | sf::Style::Resize);
 
-	while (window.isOpen()) 
-	{
-        while (const std::optional event = window.pollEvent())
-        {
+	while (window.isOpen())  {
+		// Events related to the window itself
+		while (const std::optional event = window.pollEvent()) {
 			if (event->is<sf::Event::Closed>()) {
 				window.close();
 			}
-        }
+			else if (event->is<sf::Event::Resized>()) {
+				std::cout << "The window has been resized!\n";
+			}
+		}
 	}
-
 	return 0;
 }
